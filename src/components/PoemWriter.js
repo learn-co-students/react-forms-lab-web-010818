6e790b5195d3fd0348e02f32bea57d852bd34abf
Raskovan <alexey.katalkin@gmail.com>
2018-03-14 17:21:29 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,44 @@
 import React from "react";
 
+const isValidPoem = content => {
+  let lines = content.split("\n").map(line => line.trim())
+  if (lines.length === 3) {
+    return (
+      lines[0].split(" ").length === 5 &&
+      lines[1].split(" ").length === 3 &&
+      lines[2].split(" ").length === 5
+    )
+  } else {
+    return false;
+  }
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: "",
+      isValid: true
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      poem: event.target.value,
+      isValid: isValidPoem(event.target.value)
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea rows="3" cols="60" onChange={this.handleChange}/>
+        {!this.state.isValid ?
+        (<div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
-        </div>
+        </div>) : null
+        }
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleChange = event => {
+    // debugger
+    // let charLeft = this.props.maxChars - event.target.value.length
+    // console.log(charLeft);
+
+    this.setState({
+    value: event.target.value,
+  });
   }
 
   render() {
+    // debugger
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <span>{this.props.maxChars - this.state.value.length}</span>
       </div>
     );
   }
